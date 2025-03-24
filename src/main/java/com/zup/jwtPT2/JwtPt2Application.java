f2312fe65448601931f056f1276bbb45230a9b7f
laurazup <laura.rodrigues@zup.com.br>
2025-03-24 20:07:53 -0300

    first commit

---
+++ src/main/java/com/zup/jwtPT2/JwtPt2Application.java
@@ -0,0 +1,13 @@
+package com.zup.jwtPT2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtPt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtPt2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zup/jwtPT2/JwtPt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.jwtPT2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtPt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
